80e75a98747cb815669ddea5708c43b045400054
BMPS <kyawmyohtun15396@gmail.comgit config --global user.email kyawmyotun115396@gmail.com>
2021-12-06 13:58:12 +0630

    route with controller

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WebFrontend\AboutController;
+use App\Http\Controllers\WebFrontend\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about',[AboutController::class,'aboutPage']);
+
+Route::get('contact',[ContactController::class,'contactPage']);
+
+Route::get('products/{product_id}/shop/{shop_id}',[ProductController::class,'productPage']);
+
+Route::get('shops',function(){
+    return view("shops.shop");
+});
+Route::get('products',function(){
+    return view('product');
+});

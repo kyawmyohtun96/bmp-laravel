1ec0aa4a99e512359757960d529368a54decc9f1
BMPS <kyawmyohtun15396@gmail.comgit config --global user.email kyawmyotun115396@gmail.com>
2021-12-06 21:24:46 +0630

    view of Extend and Include

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>contact</title>
-</head>
-<body>
- <h1>welcome contact page</h1>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title','contact')
+
+@section('content')
+    <h2>This is contact</h2>
+@endsection

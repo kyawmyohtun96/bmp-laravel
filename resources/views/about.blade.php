d0ea664b184286997101811442b5e060bd8d7e67
BMPS <kyawmyohtun15396@gmail.comgit config --global user.email kyawmyotun115396@gmail.com>
2021-12-13 15:30:19 +0630

    link js,css and image by asset or url

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,21 @@
 
 @section('title','about')
 
-@section('content')
-    <h2>This is about</h2>
+@section('css')
+{{-- you can link by asset or url --}}
+<link rel="stylesheet" href="{{url('css/about.css')}}">
 @endsection
+
+
+@section('content')
+
+    <h2>This is about</h2>
+    <img src="{{url('image/images.jpg')}}" alt="image">
+
+@endsection
+
+@section('js')
+{{-- you can link by asset or url --}}
+<script src="{{asset('js/about.js')}}"></script>
+@endsection
+
